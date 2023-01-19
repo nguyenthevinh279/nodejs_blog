c8088aaf87e67f59b46ac554403ef022244ce4af
nguyenthevinh279 <ntvinhkts@gmail.com>
2023-01-19 20:59:16 +0700

    Install Morgan

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
+app.use(morgan('combined'))
 
 app.get('/trang-chu', (req, res) => {
-    var a=1;
-    var b=2;
-    var c=a+b;
+    
 
  return res.send('Hello World!')
 })
