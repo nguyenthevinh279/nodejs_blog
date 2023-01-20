e958231899c19ad9d00ade97c0210288bc2844d4
nguyenthevinh279 <ntvinhkts@gmail.com>
2023-01-20 15:40:08 +0700

    Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ const { engine } = require ('express-handlebars');
 
 const app = express()
 const port = 3000
+const route=require('./routes')
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use(morgan('combined'))
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// app.use(morgan('combined'))
 
 app.engine('hbs', engine({
   extname: '.hbs'
@@ -18,13 +24,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// route init
+route(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-    res.render('news');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
